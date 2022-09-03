ff7cfd4de03a1eca0ae806b996ecbd73ea78458b
JamesJPorter <porterjjames@gmail.com>
2022-09-02 19:48:57 -0700

    built first quiz question, no answers

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,12 +1,12 @@
 // grabbing elements from HTML
-var countdown = document.getElementById("#countdown");
-var start = document.getElementById("#start");
-var quizContainer = document.getElementById("#quizContainer");
-var h2El = document.getElementById("#quizHeader");
-var quiz = document.getElementById("#quiz");
+var countdown = document.getElementById("countdown");
+var start = document.getElementById("start");
+var quizContainer = document.getElementById("quizContainer");
+var h2El = document.getElementById("quizHeader");
+var quiz = document.getElementById("quiz");
 
 // Creating elements to be used in live quiz
-var h2El = document.getElementById("#quizHeader");
+var h2El = document.getElementById("quizHeader");
 var quizQuestionEl = document.createElement("h3");
 var listeEl = document.createElement("ol");
 
@@ -22,9 +22,14 @@ btn1.value = "";
 
 var timer;
 
+// adding text & styles to elements
+h2El.textContent = "Welcome! Click below to begin the quiz";
+quizContainer.setAttribute("style", "margin: 5rem; border: 3px solid black; padding: 2rem; width: 40%; max-height: 50%; border-radius: 50px; background-color: #ffffc7ff; display: flex; justify-content: center; align-items: center; flex-direction: column;")
+
 function runQuiz() {
-    start.preventDefault();
-    h2E1.textContent = "What is the purpose of a function in JavaScript?"
+    // start.preventDefault();
+    quizContainer.appendChild(quizQuestionEl)
+    quizQuestionEl.textContent = "What is the purpose of a function in JavaScript?"
 }
 
-start.addEventListener("click", runQuiz());
+start.addEventListener("click", runQuiz);
